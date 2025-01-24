60fe8802e96a30382d9196931ac0584d3a7bc6c2
Aminenur Gökmen <93777622+aminenurgokmen@users.noreply.github.com>
2025-01-25 01:53:21 +0300

    add

--- Assets/Scripts/ParticleScript.cs
+++ Assets/Scripts/ParticleScript.cs
@@ -21,22 +21,22 @@ public class ParticleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lerpValue += Time.deltaTime*.1f;
+        lerpValue += Time.deltaTime * .1f;
 
-        var xPos = Mathf.Clamp(transform.position.x ,-clampPos, clampPos);
-        var zPos = Mathf.Clamp(transform.position.z, clampPos, clampPos*2);
-        transform.position = new Vector3(xPos,transform.position.y,zPos);
+        var xPos = Mathf.Clamp(transform.position.x, -clampPos, clampPos);
+        var zPos = Mathf.Clamp(transform.position.z, clampPos, clampPos * 2);
+        transform.position = new Vector3(xPos, transform.position.y, zPos);
         if (glasScript.isOpen)
         {
             onFloor = true;
         }
         if (destroy)
         {
-            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one*0.12f, lerpValue*.3f);
+            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * 0.1f, lerpValue * .3f);
 
         }
     }
-    
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "floor")
@@ -51,11 +51,11 @@ public class ParticleScript : MonoBehaviour
         objectRenderer.sharedMaterial = newColor;
     }
     public IEnumerator DestroyParticles()
-    {     
+    {
         yield return new WaitForSecondsRealtime(1.1f);
         destroy = true;
         GetComponentInChildren<ParticleSystem>().Play();
-        Destroy(gameObject,1f);
+        Destroy(gameObject, 1f);
 
     }
 
