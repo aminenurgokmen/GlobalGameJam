902e5a59fa1dd030025fc4b1878b942310275f3e
Aminenur Gökmen <93777622+aminenurgokmen@users.noreply.github.com>
2025-01-24 21:20:56 +0300

    L buyuk olcak

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -5,12 +5,12 @@ public class TestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.log("Hello World!");
+        Debug.Log("Hello World!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
