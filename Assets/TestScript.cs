60233003ff6497f5f1c915acee85ec23b3a5c2cb
Alp Kağan Aygün <62354598+AygunAlpKagan@users.noreply.github.com>
2025-01-24 21:11:32 +0300

    first commit 🐶

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -5,7 +5,7 @@ public class TestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.log("Hello World!");
     }
 
     // Update is called once per frame
